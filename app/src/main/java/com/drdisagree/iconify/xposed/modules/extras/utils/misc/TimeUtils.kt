4bf72f97cf874412577b8d9530353a99db716c10
GamesPuten <jamesiauten@outlook.com>
2026-07-10 10:10:10 -0400

    Fix hour index calculation for 12-hour format
    now will it blend?!

--- app/src/main/java/com/drdisagree/iconify/xposed/modules/extras/utils/misc/TimeUtils.kt
+++ app/src/main/java/com/drdisagree/iconify/xposed/modules/extras/utils/misc/TimeUtils.kt
@@ -78,7 +78,9 @@ object TimeUtils {
             }
         } else {
             try {
-                getHourhhString(modRes, hour.toInt()) // Use getHourhhString for 12-hour format
+                val hourInt = hour.toInt()
+                val hourIndex = if (hourInt == 0) 11 else hourInt - 1
+                getHourhhString(modRes, hourIndex) // Use getHourHHString for 12-hour format
             } catch (t: Throwable) {
                 getNumberString(modRes, hour.toInt()) // Fallback to getNumberString
             }
